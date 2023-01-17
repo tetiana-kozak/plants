7ade8de0909636cd81846c02ad8144ca51a6512f
Tetiana <kozaktanya077@gmail.com>
2023-01-17 22:40:31 +0200

    add: slider to What Clients Saying section

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
-  $(".slider").slick({
+  $(".slider-staff").slick({
     dots: true,
     infinite: false,
+    arrows: false,
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 4,
@@ -23,6 +24,30 @@ $(document).ready(function () {
           infinite: true,
         },
       },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+});
+
+$(document).ready(function () {
+  $(".slider-feedbacks").slick({
+    dots: true,
+    infinite: true,
+    arrows: false,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    responsive: [
       {
         breakpoint: 600,
         settings: {
