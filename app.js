0d6c8a21b88c51a7adf0daa1622acf9ea88bcaae
Tetiana <kozaktanya077@gmail.com>
2023-02-11 11:48:09 +0200

    add articles section

--- app.js
+++ app.js
@@ -67,6 +67,32 @@ $(document).ready(function () {
   });
 });
 
+//slider Articles
+
+$(document).ready(function () {
+  $(".articles-slider").slick({
+    dots: true,
+    infinite: true,
+    arrows: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+});
+
 // FAQ spoiler
 
 function spoiler() {
